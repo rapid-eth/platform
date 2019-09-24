ee026be99798e603ff9d3cad375302c888a97ade
Kames <info@kamescg.com>
2019-09-24 10:22:08 -0400

    faucet quest component

---
+++ packages/applications/consenquest/src/components/QuestRinkebyFaucet.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from "@reach/router"
+import { Button, Box, Flex, Heading, Span, Form, Field } from '@horizin/design-system';
+
+const QuestRinkebyFaucet = ({ box, threadName, ...props }) => {
+  const onSubmit = async (values) => {
+    const url = 'https://brn68gkbcf.execute-api.us-east-1.amazonaws.com/cors/verify-rinkeby-faucet-tx'
+    try {
+
+      const response = await fetch(url, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(values)
+      });
+      console.log(response)
+      if (response.json) {
+        const JSONResponse = await response.json();
+        console.log(JSONResponse)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  return (
+    <Form callback={onSubmit}>
+      <Field
+        name="transactionHash"
+        placeholder="Transaction Hash"
+      />
+      <Field
+        name="address"
+        placeholder="Address"
+      />
+      <Button sm type="submit" variant='green'>Submit Evidence</Button>
+    </Form>
+  );
+}
+
+export default QuestRinkebyFaucet
--- packages/applications/consenquest/src/components/index.js
+++ packages/applications/consenquest/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Branding } from './Branding'
 export { default as Footer } from './Footer'
 export { default as MenuDefault } from './MenuDefault'
+export { default as QuestRinkebyFaucet } from './QuestRinkebyFaucet'
