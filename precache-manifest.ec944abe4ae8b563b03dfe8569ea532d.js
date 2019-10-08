8b15122c4608f0369a4c8acaf28346f9c8ad339b
Kames <info@kamescg.com>
2019-10-08 16:22:23 -0400

    Updates

---
+++ precache-manifest.ec944abe4ae8b563b03dfe8569ea532d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a0e75970307f3798807058c18629d1",
+    "url": "/platform/index.html"
+  },
+  {
+    "revision": "cd84d8c815491d94b36c",
+    "url": "/platform/static/css/main.4da201b2.chunk.css"
+  },
+  {
+    "revision": "efb92e838320200e0531",
+    "url": "/platform/static/js/2.673645d7.chunk.js"
+  },
+  {
+    "revision": "cd84d8c815491d94b36c",
+    "url": "/platform/static/js/main.5bc150d8.chunk.js"
+  },
+  {
+    "revision": "4160f3df5715ad3b09de",
+    "url": "/platform/static/js/runtime~main.dfefaae4.js"
+  }
+]);
