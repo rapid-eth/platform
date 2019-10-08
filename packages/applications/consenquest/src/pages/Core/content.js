a2129eee3090dc3a72574a47c27f0cbd47b05075
orbro <billy@consensys.net>
2019-10-08 14:22:03 -0700

    content updates

--- packages/applications/consenquest/src/pages/Core/content.js
+++ packages/applications/consenquest/src/pages/Core/content.js
@@ -3,18 +3,25 @@
 /* -------------------- */
 export const Team = `
 # Project Overview
-**Build a decentralized application for Ethereum developers to connect and collabroate.** Enable sharing and guidance via a decentralized content management system. Plus, include offset gas costs for redeeming rewards to simplify onboarding experience.
 
-- Features: Social Curation of Quests
-- Timeline: 2 Weeks
-- Requirements: Rewards & Referrals
+### Mission
+
+**Help grow the Ethereum ecosystem to 1 million developers by introducing new developers to their blockchain 'a-ha' moment**
+
+### Our Approach
+
+**Build a 'Questing' platform that make the experience of 'a-ha' moments fun and easy.** Make the platform extensible enough to allow the community to easily create their own Quests. Make the platform not only interesting for new Ethereum developers, but also for those already involved in the space - to keep them around - by creating a platform which allows them to connect and collaborate.
+
+- **Main Features:** A curated 'A-Ha' Moment questing flow
+- **Additional Features:** Rewards & Referrals
+- **Timeline:** 2 Weeks
 
 ## Mission Accepted
 
 <br/>
 <br/>
 # The Rapid Team
-The Rapid team is a small Research and Development team within ConsenSys. We focus on building prototypes and minimal viable products using decentralized technology.
+The Rapid team is a small Research and Development team within ConsenSys. We focus on building prototypes and minimal viable products on Ethereum using decentralized technology.
 
 ### Project Notes
 For the <strong>One Million Ethereum</strong> campaign we decided to push the limits of decentralized Authentication, Storage and Messaging. 3Box provides a suite of decentralized identity tools to enable rapid building of social applications.
@@ -36,8 +43,11 @@ How cool is that?
 ## Billy Luedkete
 #### Product Manager
 
+The gamification of educational or instructional material is not an entirely new concept; however, it is a concept which Ethereum makes immensely more valuable. In Web2.0 game platforms, the rewards you obtain and reputation you build are not portable - they do not exist outside of that specific game's platform. What if that platform becomes obsolete or unsupported? What if you want to show your achievements to the outside world?
 
+By leveraging Ethereum, we can make an entirely new class of gamification platforms which can all easily interoperate with applications that have nothing to do with the games themselves. Composability at its finest.
 
+This application is the first step in that direction.
 `
 
 /* -------------------- */
@@ -45,11 +55,11 @@ How cool is that?
 /* -------------------- */
 export const Feedback = `
 # How Are We Doing
-**What do you think of the experiment?** *Do you like it? Love it!? Let us know.*
+**What do you think of the experiment?** *Do you like it? Love it!? Let us know!*
 
-The website uses IPFS for **content management**, Decentralized Identity for **serverless authentication** and Ethereum blockchain to manage **peer-to-peer rewards**. In other words it's still a **Beta project.**
+The website uses IPFS for **content management**, Decentralized Identity for **serverless authentication** and Ethereum blockchain to manage **peer-to-peer rewards**. In other words, it's still a **Beta project!**
 
-Find a bug? Missing an important feature. **Let us know!**
+Find a bug? Missing an important feature? **Let us know!**
 
 `
 
@@ -62,8 +72,17 @@ export const Contribute = `
 
 Help build the foundation for sustainble online communities.
 
-The One Million Developers infrastructure is interfacte agnostic. Built using 3Box Storage and Threads all of the dynamic content is stored and retrieved from the decentralized cloud.
+The One Million Developers infrastructure is interface agnostic. Built using 3Box Storage and Threads all of the dynamic content is stored in and retrieved from the decentralized cloud.
 
+We want this to be an open, collaborative effort - contribute directly to the open source repo at https://rapid-eth.github.io/platform, or build standalone extensions - the world is your oyster.
+
+**Not a Developer?**
+
+Don't worry! You'll soon be able to create your own Quests and Adventures without needing to write a single piece of code!
+
+**Can't Wait?
+
+Reach out to the RAPID team at rapid@consensys.net to get onto the approved list of Adventure Creators, or to get your quests added to the initial Adventures we've established!
 `
 
 /* -------------------- */
@@ -90,18 +109,15 @@ Pellentesque non odio nibh. Mauris erat nunc, egestas ac eros eu, facilisis temp
 /* How It Works
 /* -------------------- */
 export const HowItWorks = `
-# Etheruem the Blockchain
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi accumsan felis lacus, id faucibus erat lacinia nec. Nullam luctus justo ac sem rutrum tincidunt. Suspendisse tempor, dui condimentum semper aliquet, dui dolor accumsan lorem, cursus porta metus eros at nunc. Integer a viverra nibh. Nunc condimentum libero ex. Aliquam erat volutpat. Mauris convallis dapibus mi non volutpat. Curabitur sagittis interdum ligula, in porta tortor pretium in. Proin vel tincidunt felis. Curabitur vitae nibh magna. Ut id tellus a ante vulputate mollis. Etiam finibus pellentesque eros ut maximus. Curabitur consectetur sit amet tortor nec pellentesque. Integer vitae urna massa.
+# Purpose
+We believe that Ethereum’s success is highly dependent on the number of individuals involved in the ecosystem. Our hope is to provide a platform for the creation of, and interaction with, Quests/Adventures - which are interactive tasks designed to give people their blockchain ‘a-ha!’ moment(s). While the first version of this platform consists of 4 adventures established by the RAPID team, the next step will be enabling the community at large to build their own adventures using the tools we provide.
 
-# Etheruem the Technology
-Sed ornare tincidunt dui, et facilisis ipsum tempus ac. Fusce suscipit id augue id egestas. Donec tempor, libero placerat pulvinar congue, nulla est volutpat quam, id volutpat ipsum felis at mauris. In scelerisque non risus aliquam pretium. Nunc condimentum enim id malesuada ullamcorper. Maecenas id rhoncus neque, vitae efficitur lacus. Quisque eleifend magna nec turpis molestie, eu dignissim mi aliquam. Suspendisse sit amet aliquet eros. Cras a felis commodo, cursus sem ut, auctor lectus. In egestas rhoncus scelerisque. Mauris viverra sed sapien sed feugiat. Fusce id ligula tristique purus accumsan gravida eget sed augue. Quisque eu lorem suscipit sapien auctor euismod eget vel elit. Praesent sit amet arcu eget neque blandit ultricies.
+# The Adventure
+Each Adventure is centered around a specific theme, and is comprised of multiple Quests. To complete a Quest, you will be required to perform the action outlined in the Quest’s description. Once complete, if the Quest involves a reward, you will submit evidence of quest completion (either your Ethereum address or a transaction hash) and receive a token certificate as a reward, if you’ve indeed completed the respective action. A token certificate is something developed by the RAPID team which allows the recipient of the certificate to redeem it for the underlying rewards (tokens!). Don’t worry too much about how this works (unless you’re a developer - then head over to our documentation to learn more!) - our platform will walk you through the process!
 
-# Etheruem the Community
-Ut tristique risus eget dolor imperdiet, in auctor ipsum commodo. Nulla facilisi. Cras vulputate, dolor a convallis tincidunt, mauris dolor placerat mauris, eget mollis lorem tellus molestie velit. Nulla consectetur est nec ipsum luctus mollis. Aenean ac urna porta, elementum odio vel, volutpat sapien. Vivamus dignissim ligula est, at tincidunt est dignissim vel. Nam risus tellus, gravida sit amet arcu sed, elementum venenatis urna. Praesent ac sollicitudin mauris.
+# The Rewards
+By completing Quests, you’ll receive tokens! Initially, the tokens you receive will be DEV tokens. Only 1 million DEV tokens will ever be created; however, their value is yet unknown (besides their reputational value!). The main purpose of DEV token is to demonstrate the potential of the 1MD questing platform - in the future, when the platform is made more open and extensible, you’ll be able to create your own adventures, your own quests, and reward people with your own tokens!
 
-# Etheruem the Cryptocurrency
-Ut tristique risus eget dolor imperdiet, in auctor ipsum commodo. Nulla facilisi. Cras vulputate, dolor a convallis tincidunt, mauris dolor placerat mauris, eget mollis lorem tellus molestie velit. Nulla consectetur est nec ipsum luctus mollis. Aenean ac urna porta, elementum odio vel, volutpat sapien. Vivamus dignissim ligula est, at tincidunt est dignissim vel. Nam risus tellus, gravida sit amet arcu sed, elementum venenatis urna. Praesent ac sollicitudin mauris.
-
-# Etheruem the Adventure
-Pellentesque non odio nibh. Mauris erat nunc, egestas ac eros eu, facilisis tempus nisl. Vestibulum at ante id est venenatis luctus. Nam suscipit mauris ac justo scelerisque congue. Maecenas eu tortor vestibulum, accumsan lorem id, euismod nisi. Nullam volutpat eu risus ut tristique. In mattis sagittis odio, vel posuere felis. Etiam non condimentum massa. Curabitur augue diam, feugiat ut accumsan et, venenatis ac risus. In ut porta orci. Suspendisse eu odio in orci pharetra tincidunt a vel sapien. Mauris commodo laoreet leo, at iaculis nunc interdum ac.
+# Vision of the Future
+As mentioned, our goal is to turn this platform into a tool which allows individuals / teams from around the ecosystem to easily create their own quests. Through this, we’ll end up with a community-curated educational/informational experience that can introduce individuals from both inside and outside of the ecosystem to all of the amazing things happening on Ethereum. For example, a MakerDAO adventure might be created by the MakerDAO team to introduce users to their platform. Alternatively, a user may create an overarching “DeFi” adventure which includes interaction with MakerDAO. These adventures/quests will be both created and ranked by the community, making this an extremely collaborate initiative which needs your help to succeed!
 `
--- packages/applications/consenquest/src/pages/Guided/Start.jsx
+++ packages/applications/consenquest/src/pages/Guided/Start.jsx
@@ -13,7 +13,7 @@ const Page = ({ styled, ...props }) => {
           <Span>Those</Span>
           <Span xxl>💡</Span>
           <Span>moments in</Span>
-          <Image src='https://cdn4.iconfinder.com/data/icons/cryptocoins/227/ETH-512.png' maxWidth={30}/>
+          <Image src='https://cdn4.iconfinder.com/data/icons/cryptocoins/227/ETH-512.png' maxWidth={30} />
         </Flex></Heading>
         <Flex center column>
           <Heading xxl heavy>The Ethereum Journey</Heading>
@@ -21,7 +21,7 @@ const Page = ({ styled, ...props }) => {
         </Flex>
         <Box textCenter>
           <HorizontalRule />
-            <Span xs my={3}>Currently there are 6,723 developers working on Ethereum. Be counted by signing your name with Github or 3Box.</Span>
+          <Span xs my={3}>Currently there are 6,723 developers working on Ethereum. Be counted by signing your name with Github or 3Box.</Span>
           <HorizontalRule />
         </Box>
       </Container>
@@ -29,73 +29,55 @@ const Page = ({ styled, ...props }) => {
       <Container>
         <Flex>
           <Box flex={1} p={4}>
-          <Heading heavy>Welcome to Ethereum</Heading>
-          <Paragraph>
-            Ethereum. The global, open-source platform for decentralized applications, Ethereum is one of the most revolutionary technologies of our time - and the world’s leading programmable blockchain.
-          </Paragraph>
+            <Heading heavy>Welcome to Ethereum</Heading>
+            <Paragraph>
+              Ethereum. Those who understand it won’t stop talking about it. Its following is almost cult-like - people who dive down the rabit hole seem to never see the world the same again. Those who are indoctrinated tend to never look back.
+            </Paragraph>
 
             <Paragraph>
               At its core, Ethereum is the foundation for a new era of the internet:
             </Paragraph>
-            <ul>
-              <li>An internet where money and payments are built in.</li>
-              <li>An internet where users can own their data, and your apps don’t spy and steal from you.</li>
-              <li>An internet where everyone has access to an open financial system.</li>
-              <li>An internet built on neutral, open-access infrastructure, controlled by no company or person.</li>
-            </ul>
             <Paragraph>
-              Like other blockchains, Ethereum has a native cryptocurrency called Ether (ETH). ETH is digital money. If you’ve heard of Bitcoin, ETH has many of the same features. It is purely digital, and can be sent to anyone anywhere in the world instantly.
-            </Paragraph>
-            <Paragraph>
-              The supply of ETH isn’t controlled by any government or company - it is decentralized, and it is scarce. People all over the world use ETH to make payments, as a store of value, or as collateral.
+              Because it really is THAT cool. Because it really is THAT revolutionary. Because as soon as you have your “holy shit” Ethereum moment, there is no going back. As soon as you understand this technology’s potential, you’ll most likely drop what you’re doing and join the movement.
             </Paragraph>
 
           </Box>
           <Box flex={1} p={4}>
-            <Heading heavy>A Different Approach</Heading>
+            <Heading heavy>The Adventure</Heading>
             <Paragraph>
-              But unlike other blockchains, Ethereum can do much more. Ethereum is programmable, which means that developers can use it to build new kinds of applications.
+              The goal of this platform is to attempt to give you that “holy shit” moment. Although it typically takes quite a while to wrap your head around everything, we’ve tried to condense the experience into a nice little interactive package.
             </Paragraph>
 
             <Paragraph>
-              These decentralized applications (or “dapps”) gain the benefits of cryptocurrency and blockchain technology. They can be trustworthy, meaning that once they are “uploaded” to Ethereum, they will always run as programmed. They can control digital assets in order to create new kinds of financial applications. They can be decentralized, meaning that no single entity or person controls them.
+              This is a difficult task, so we may not succeed. However, we hope that we spark your interest enough to get you to do some more research on your own, so that you may fully understand the technology and its potential.
             </Paragraph>
 
             <Paragraph>
-              Right now, thousands of developers all over the world are building applications on Ethereum, and inventing new kinds of applications, many of which you can use today:
+              Welcome to Ethereum. Enjoy the adventure.
             </Paragraph>
 
-            <Paragraph>
-              Cryptocurrency wallets that let you make cheap, instant payments with ETH or other assets
-              Financial applications that let you borrow, lend, or invest your digital assets
-              Decentralized markets, that let you trade digital assets, or even trade “predictions” about events in the real world
-              Games where you own in-game assets, and can even make real money
-              And much, much more.
-            </Paragraph>
           </Box>
 
           <Box flex={1} p={4}>
-          <Heading heavy>Large Community</Heading>
-          <Paragraph>
-            The Ethereum community is the largest and most active blockchain community in the world. It includes core protocol developers, cryptoeconomic researchers, cypherpunks, mining organizations, ETH holders, app developers, ordinary users, anarchists, fortune 500 companies, and, as of now, you.
-          </Paragraph>
+            <Heading heavy>Example 'a-ha' Moment</Heading>
+            <Paragraph>
+              <strong><i>Back when I was first learning Ethereum, one of the first things I thought was really cool about it was just that, woah, I don't have to run my own servers. I don't have to provision my own servers, I don't have to maintain servers—it's just Ethereum, and it's always there. I thought that was really cool on its own.</i></strong>
+            </Paragraph>
 
-          <Paragraph>
-            There is no company or centralized organization that controls Ethereum. Ethereum is maintained and improved over time by a diverse global community of contributors who work on everything from the core protocol to consumer applications. This website, just like the rest of Ethereum, was built - and continues to be built - by a collection of people working together.
-          </Paragraph>
+            <Paragraph>
+              Mike Goldin, <br />
+              ConsenSys
+            </Paragraph>
 
-          <Paragraph>
-            <strong>Welcome to Ethereum.</strong>
-          </Paragraph>
           </Box>
         </Flex>
       </Container>
 
       <Flex center column>
         <Box p={6} bg='dark' color='white' borderRadius={60} maxWidth={760} overflow='hidden' textCenter>
-        <BackgroundImage
-                   ratio={.75} opacity={.2}
-                  src='https://cdn.dribbble.com/users/1780890/screenshots/5672197/illu1_1x_4x.jpg' />
+          <BackgroundImage
+            ratio={.75} opacity={.2}
+            src='https://cdn.dribbble.com/users/1780890/screenshots/5672197/illu1_1x_4x.jpg' />
           <Heading xl textCenter noMargin>Dive into Ethereum</Heading>
           <Paragraph >
             <em>Just getting started?</em> <strong> Start from the very beginning.</strong>
